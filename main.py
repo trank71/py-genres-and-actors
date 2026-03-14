1b8a68df8ec8bf2a8c39f5ca43eb2ffe31839f4e
Ambra <trank13f@gmail.com>
2026-03-14 16:19:59 +0700

    created CREATE manager

--- main.py
+++ main.py
@@ -2,6 +2,44 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Actor, Genre
+
 
 def main() -> QuerySet:
-    pass
+    Genre.objects.create(
+        name="Western",
+    )
+    Genre.objects.create(
+        name="Action"
+    )
+    Genre.objects.create(
+        name="Dramma"
+    )
+    Actor.objects.create(
+        first_name="George",
+        last_name="Klooney",
+    )
+    Actor.objects.create(
+        first_name="Kianu",
+        last_name="Reaves",
+    )
+    Actor.objects.create(
+        first_name="Scarlett",
+        last_name="Keegan",
+    )
+    Actor.objects.create(
+        first_name="Will",
+        last_name="Smith",
+    )
+    Actor.objects.create(
+        first_name="Jaden",
+        last_name="Smith",
+    )
+    Actor.objects.create(
+        first_name="Scarlett",
+        last_name="Johansson",
+    )
+
+
+if __name__ == "__main__":
+    main()
