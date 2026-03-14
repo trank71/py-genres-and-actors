ef83a3757cd9fd3dcd396d1ddff00ab5b80ce939
Ambra <trank13f@gmail.com>
2026-03-14 17:02:54 +0700

    change CREATE from list to typle in actors

--- main.py
+++ main.py
@@ -7,19 +7,18 @@ from db.models import Actor, Genre
 
 def main() -> QuerySet:
     genres = ["Western", "Action", "Dramma"]
-    actors = ["George Klooney", "Kianu Reaves"
-              , "Scarlett Keegan", "Will Smith"
-              , "Jaden Smith", "Scarlett Johansson"
+    actors = [("George", "Klooney"), ("Kianu", "Reaves")
+              , ("Scarlett", "Keegan"), ("Will", "Smith")
+              , ("Jaden", "Smith"), ("Scarlett", "Johansson")
               ]
 
     for genre in genres:
         Genre.objects.create(name=genre)
 
-    for actor_name in actors:
-        actor = actor_name.split()
+    for first_name, last_name in actors:
         Actor.objects.create(
-            first_name=actor[0],
-            last_name=actor[1]
+            first_name=first_name,
+            last_name=last_name,
         )
 
     Genre.objects.filter(name="Dramma").update(
